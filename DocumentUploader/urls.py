1c027dacb5be20f001e1edd5ecc4de9a16c8c50e
ashvin-007 <parmarashvin9280@gmail.com>
2025-02-24 17:49:53 +0530

    Fix homepage URL

--- DocumentUploader/urls.py
+++ DocumentUploader/urls.py
@@ -23,5 +23,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('doc/',include('doc.urls')),
+    path('',include('doc.urls')),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
